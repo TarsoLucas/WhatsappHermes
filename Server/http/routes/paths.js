22363c01085ebd3204ea8ee9597292949e061074
Tech-Tarso <oliveiratls@agilize.com.br>
2025-07-11 08:51:35 -0300

    Fix: Desabilitar rotas de banco temporariamente

--- Server/http/routes/paths.js
+++ Server/http/routes/paths.js
@@ -4,11 +4,11 @@ const router = express.Router();
 // Importando as rotas específicas
 const hermesRoutes = require('./hermesRoutes');
 const fileRoutes = require('./fileRoutes');
-const contatosRoutes = require('./contatosRoutes');
+// const contatosRoutes = require('./contatosRoutes'); // Temporariamente desabilitado
 
 // Usando as rotas
 router.use('/hermes', hermesRoutes);
 router.use('/files', fileRoutes);
-router.use('/contatos', contatosRoutes);
+// router.use('/contatos', contatosRoutes); // Temporariamente desabilitado
 
 module.exports = router;
